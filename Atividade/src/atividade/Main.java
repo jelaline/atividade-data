93b15162dee6bf45af2d2bf3ecd24ccb60804405
Jessica Aline <134285959+jelaline@users.noreply.github.com>
2023-08-21 10:17:47 -0300

    Add files via upload

---
+++ Atividade/src/atividade/Main.java
@@ -0,0 +1,20 @@
+
+package atividade;
+
+/**
+ *
+ * @author Jessica
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+        Data data = new Data(12, 3, 2021);
+        
+        data.validaData();
+        data.mostrarData();
+        
+        
+       
+    }
+    
+}
